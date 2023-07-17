4af327d8827569b272e9537f8df2e5eb8fe20c66
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-17 10:53:09 -0500

    Se hacen cambios en la rama master

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Tengo hambre y sueño")
-print("¿Por qué me persigue la desgracia :(")
-print("En la rama desarrollo")
+print("Tengo y sueño")
+print("Otra linea")
+print("¿Por qué me persigue la desgracia")
+print("En la rama Main")
